8e2c1f7029111222f222199ab7fd568261e58208
Andrei <39272243+andreicville@users.noreply.github.com>
2020-07-27 09:54:30 -0400

    Update Arrays_NewYearChaos.java

--- Arrays_NewYearChaos.java
+++ Arrays_NewYearChaos.java
@@ -44,9 +44,30 @@ package HackerRank1;
 //Too chaotic
 public class Arrays_NewYearChaos {
 
-	public static void main(String[] args) {
-	
-
-	}
+	public static void main(String []args){
+         int[] a = {2,1,5,3,4};
+         
+        System.out.println(minB(a));
+     }
+     public static String minB(int[] a) {
+         int[] b = new int[a.length];
+         int counts = 0;
+         
+         System.out.println(checkArr(a));
+         
+         if (checkArr(a)) {
+            // System.out.println("Array sorted - true");
+             return "Array sorted - true";
+         }
+         else return "Too chaotic";
+        
+     }
+     
+     public static boolean checkArr(int[] a) {
+         for (int i=1; i<a.length; i++) {
+             if (a[i-1] > a[i]) return false;
+         }
+         return true;
+     }
 
 }
